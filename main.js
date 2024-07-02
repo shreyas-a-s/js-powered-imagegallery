28ba478958457319ad611fc9ebf119aa8e12df7e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-02 23:04:32 +0530

    feat: Declare arrays of image filenames and alt texts

--- main.js
+++ main.js
@@ -5,8 +5,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = [
+  'pic1.jpg',
+  'pic2.jpg',
+  'pic3.jpg',
+  'pic4.jpg',
+  'pic5.jpg'
+];
 
 /* Declaring the alternative text for each image file */
+const altTexts = {
+  'pic1.jpg': 'Close up of an eye',
+  'pic2.jpg': 'Close up of a shell',
+  'pic3.jpg': 'Some white flowers in a garden of blue ones',
+  'pic4.jpg': 'Egyptian painting of pharaoh',
+  'pic5.jpg': 'A brown butterfly sitting on an leaf'
+};
 
 /* Looping through images */
 
